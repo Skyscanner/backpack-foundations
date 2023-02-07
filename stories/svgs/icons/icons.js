6a925cc2b870fddcd400de5c2e16cae68a86a42b
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2023-02-07 14:43:58 +0000

    [KOA-5773]: Add icon stories (#187)

---
+++ stories/svgs/icons/icons.js
@@ -0,0 +1,49 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable no-undef */
+
+import './icons.scss';
+
+/**
+ *
+ * @param {String} icon path to current icon to be displayed
+ * @param {String} size which size to display the icon
+ * @param {String} name display the name of the icon
+ * @returns {Node} The html node to be displayed
+ */
+const createIconContainer = (icon, size, name) => {
+  const iconContainer = document.createElement('div');
+  iconContainer.className = 'bpk-icon-container';
+
+  const iconSwatch = document.createElement('div');
+  iconSwatch.className = 'bpk-icon-swatch';
+  iconSwatch.style.backgroundImage = `url(${icon})`;
+  iconSwatch.style.width = size;
+  iconSwatch.style.height = size;
+
+  const iconName = document.createElement('div');
+  iconName.className = 'bpk-icon-name';
+  iconName.innerText = name;
+
+  iconContainer.appendChild(iconSwatch);
+  iconContainer.appendChild(iconName);
+  return iconContainer;
+};
+
+export default createIconContainer;
