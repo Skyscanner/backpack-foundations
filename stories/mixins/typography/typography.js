1c6b2359233f19004fea10f9b5cdf41464794213
Ana Belciug <anabelciug@skyscanner.net>
2023-02-16 09:43:18 +0000

    Add typography mixins stories

---
+++ stories/mixins/typography/typography.js
@@ -0,0 +1,32 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import './typography.scss';
+
+const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+export const createTextStyleContainer = (style) => {
+    const container = document.createElement('div');
+    
+    const text = document.createElement('p');
+    text.innerText = capitalizeFirstLetter(style.replace(/-/g, " "));
+    text.className = `bpk-text bpk-text--${style}`;
+
+    container.appendChild(text);
+    return container;
+}
---
+++ stories/mixins/typography/typography.stories.js
@@ -0,0 +1,58 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createTextStyleContainer } from './typography';
+import { heroStyles, headingStyles, bodyStyles, labelStyles } from './typography-styles';
+
+export default {
+  title: 'Mixins / Typography',
+};
+
+
+const Template = (args) => {
+    const { textStyleGroup } = args;
+  
+    const storyWrapper = document.createElement('div'); // eslint-disable-line no-undef
+  
+    Object.keys(textStyleGroup).forEach((style) => {
+      const textContainer = createTextStyleContainer(textStyleGroup[style], style);
+      storyWrapper.appendChild(textContainer);
+      return storyWrapper;
+    });
+    return storyWrapper;
+  };
+  
+export const Hero = Template.bind({});
+Hero.args = {
+textStyleGroup: heroStyles,
+};
+
+export const Heading = Template.bind({});
+Heading.args = {
+textStyleGroup: headingStyles,
+};
+
+export const Body = Template.bind({});
+Body.args = {
+textStyleGroup: bodyStyles,
+};
+
+export const Label = Template.bind({});
+Label.args = {
+textStyleGroup: labelStyles,
+};
