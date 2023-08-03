70c3da875071c21386b107814c69555abef374b0
Ana Belciug <anabelciug@skyscanner.net>
2023-08-03 09:31:50 +0100

    Add aria hidden to JS icons

--- packages/bpk-svgs/dist/js/icons/lg/explore.js
+++ packages/bpk-svgs/dist/js/icons/lg/explore.js
@@ -2,7 +2,7 @@ import React from "react";
 export default (({
   styles = {},
   ...props
-}) => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{
+}) => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" aria-hidden="true" style={{
   width: "1.5rem",
   height: "1.5rem"
 }} {...props}><path d="M12 2a10 10 0 1010 10A10 10 0 0012 2zm3.962 6.748l-3.038 7.9a.55.55 0 01-.932.159.535.535 0 01-.082-.151l-.97-2.467a2 2 0 00-1.13-1.13l-2.466-.97a.535.535 0 01-.15-.081.55.55 0 01.158-.931l7.9-3.04v.001a.55.55 0 01.71.71z" /></svg>);
--- packages/bpk-svgs/dist/js/icons/lg/lightning.js
+++ packages/bpk-svgs/dist/js/icons/lg/lightning.js
@@ -2,7 +2,7 @@ import React from "react";
 export default (({
   styles = {},
   ...props
-}) => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{
+}) => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" aria-hidden="true" style={{
   width: "1.5rem",
   height: "1.5rem"
 }} {...props}><path d="M11.91 22.77a.424.424 0 01-.8-.217v-6.944a.437.437 0 00-.428-.446H5.427a.446.446 0 01-.373-.663l7.035-13.27a.424.424 0 01.8.217v6.944a.438.438 0 00.429.446h5.255a.446.446 0 01.374.663z" /></svg>);
