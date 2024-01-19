ace1e25b5f1bff1446ae669b96da355f656baba8
Matthew Robertson <matthew.robertson@skyscanner.net>
2024-01-19 11:55:15 +0000

    use correct registry

--- packages/bpk-foundations-android/gulpfile.babel.js
+++ packages/bpk-foundations-android/gulpfile.babel.js
@@ -21,11 +21,11 @@ import path from 'path';
 
 import del from 'del';
 import gulp from 'gulp';
-import theo from 'theo';
+import jsonLint from 'gulp-jsonlint';
 import gulpTheo from 'gulp-theo';
 import { flatten } from 'lodash';
 import gulpMerge from 'merge2';
-import jsonLint from 'gulp-jsonlint';
+import theo from 'theo';
 
 import transformDarkValues from '../../utils/transformDarkValues';
 
